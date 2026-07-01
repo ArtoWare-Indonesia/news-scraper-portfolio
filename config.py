6c24d264c83aface65130398064134613092dc2a
ArtoWare-Indonesia <massubi@gmail.com>
2026-07-01 23:16:43 +0700

    Add project configuration

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/137.0 Safari/537.36"
+    )
+}
 
+RSS_SOURCES = {
+    "Antara": "https://www.antaranews.com/rss/terkini.xml",
+    "Tempo": "https://rss.tempo.co/nasional",
+}
